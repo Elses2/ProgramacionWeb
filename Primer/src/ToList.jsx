e19b9422eea02ea47a66b20e560a3120bdda52b7
Elses2 <eberchiecher2@gmail.com>
2025-11-22 17:35:41 -0300

    Primer ejercicio hecho

--- Primer/src/ToList.jsx
+++ Primer/src/ToList.jsx
@@ -1,13 +1,38 @@
-export default function ToList() {
-    return (
+import React from "react";
+import "./ToList.css";
+import logo from "/favicon.png";
+const ToList = () => {    {/*Funcion flechada otra forma de hacerlo :v*/}
+  return (
+    <>
+    <div className="tolist-container">
+      <img src={logo} alt="Logo" className="tolist-logo" />
+      <h2 className="tolist-title">Elses ToList</h2>
+      <p className="tolist-description">Organiza tus tareas diarias con facilidad:</p>
+      <div className="tolist-input-row">
+        <input
+          type="text"
+          className="tolist-input"
+          placeholder="Nueva tarea..."
+          disabled
+        />
+        <button className="tolist-add-btn" disabled>Agregar tarea</button>
+      </div>
+      <ul className="tolist-list">
+        <li className="tolist-item">
+          <label className="tolist-label">
+            <input
+              type="checkbox"
+              className="tolist-checkbox"
+              disabled
+            />
+            <span className="tolist-task-text">Hacer la tarea de programacion web</span>
+          </label>
+          <button className="tolist-delete-btn" disabled>Eliminar</button>
+        </li>
+      </ul>
+    </div>
+    </>
+  );
+};
 
-        <div> 
-            <h1>To Do List</h1>
-            <ul>
-                <li>Learn React</li>
-                <li>Build a To-Do App</li>
-                <li>Master JavaScript</li>
-            </ul>
-        </ div>
-    );
-}
+export default ToList;
